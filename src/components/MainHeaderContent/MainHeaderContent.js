a6888c55f398b442db5e9b8fb9d2d09435b0d09c
Rynay <feirs911@gmail.com>
2021-06-12 19:29:22 +0300

    mainHeaderContent component

--- src/components/MainHeaderContent/MainHeaderContent.js
+++ src/components/MainHeaderContent/MainHeaderContent.js
@@ -1,5 +1,16 @@
 const MainHeaderContent = () => {
-  return <div>MainHeaderContent</div>;
+  return (
+    <section className="mainHeaderContent">
+      <h2 className="mainHeaderContent__title">Watch Joker Now</h2>
+      <p className="mainHeaderContent__text">
+        Forever alone in a crowd, failed comedian Arthur Fleck seeks connection
+        as he walks the streets of Gotham City. Arthur wears two masks -- the
+        one he paints for his day job as a clown, and the guise he projects in a
+        futile attempt to feel like he's part of the world around him.
+      </p>
+      <button className="mainHeaderContent__button">Play</button>
+    </section>
+  );
 };
 
 export default MainHeaderContent;
